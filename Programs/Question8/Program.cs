e383867182c50858a69e249537739ae72009cd31
Adarsh V <77043183+Adarshv12@users.noreply.github.com>
2023-01-19 10:46:01 +0530

    Add files via upload

---
+++ Programs/Question8/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter no of Destinations: ");
+int pSize = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the destination names: ");
+string[] passengers = new string[pSize];
+
+for(int i = 0;i < pSize; i++)
+{
+    passengers[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Enter the fares of destination: ");
+decimal[] fares = new decimal[pSize];
+
+for (int i = 0; i < pSize; i++)
+{
+    fares[i] = decimal.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Enter the nth number to display the corresponding destination and fare: ");
+int index = int.Parse(Console.ReadLine());
+
+if(index > pSize)
+{
+    Console.WriteLine("Enter valid number");
+}
+else
+{
+    Console.WriteLine($" Destination : {passengers[index]} | Fare : {fares[index]}");
+}
