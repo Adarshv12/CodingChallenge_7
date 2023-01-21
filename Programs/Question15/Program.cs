60506b9c86717051a04bb9114a5a520501f4fe93
Adarsh V <77043183+Adarshv12@users.noreply.github.com>
2023-01-21 12:02:30 +0530

    Add files via upload

---
+++ Programs/Question15/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter the overweight of 5 boarding passes");
+int[] weights = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    weights[i] = int.Parse(Console.ReadLine());
+}
+
+int sum = 0;
+int[] prices = new int[5];
+for(int i = 0;i < 5;i++)
+{
+    prices[i] = weights[i] * 15;
+    sum += prices[i];
+}
+
+Console.WriteLine("..................Extra charges for additional weights...................\n");
+
+
+Array.Reverse(prices);
+foreach (var item in prices)
+{
+    Console.WriteLine("$"+item);
+}
+Console.WriteLine($"Total amount = ${sum}");
