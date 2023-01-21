3a2115a12966de9f5ae52fadb74fddf573cb717c
Adarsh V <77043183+Adarshv12@users.noreply.github.com>
2023-01-21 11:59:54 +0530

    Add files via upload

---
+++ Programs/Question12/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Enter 3 passengers names:");
+string[] passengers = new string[6];
+
+for(int i = 0;i < 3; i++)
+{
+    passengers[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Enter 3 destination:");
+string[] destination = new string[3];
+
+for (int i = 0; i < 3; i++)
+{
+    destination[i] = Console.ReadLine();
+}
+
+Console.WriteLine("....................Merging into passenger Array...........................");
+
+int j = 3;
+
+for (int i = 0;i < 3; i++)
+{
+    passengers[j] = destination[i];
+    j++;
+}
+Console.WriteLine("Array after merging: ");
+
+foreach (var item in passengers)
+{
+    Console.WriteLine(item);
+}
